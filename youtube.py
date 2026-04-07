269504bda5dfc01eb663973921a417edb031a11a
AK <aamaal00777@gmail.com>
2026-04-07 17:29:11 +0400

    Implmented function to change metadata of mp3

--- youtube.py
+++ youtube.py
@@ -1,8 +1,12 @@
+import time
 from pathlib import Path
+
 from pydub import AudioSegment
 from pytubefix import YouTube
 from pytubefix import Playlist
 import os
+from mutagen.mp4 import MP4
+import re
 
 class EbutouyDownloader():
     def __init__(self, link: str, path: str = str(Path.home() / "Downloads")):
@@ -26,6 +30,13 @@ class EbutouyDownloader():
             self.thumbnail = self.yt.thumbnail_url
 
 
+
+    def sanitize_filename(self, filename):
+        """Remove invalid characters from filename."""
+        # Remove quotes, slashes, colons, etc.
+        invalid_chars = r'[<>:"/\\|?*]'
+        return re.sub(invalid_chars, '', filename)
+
     def video_download(self, resolution):
         if self.yt:
             self.streams = self.yt.streams.filter(progressive=True, res=resolution, file_extension='mp4').first()
@@ -46,7 +57,7 @@ class EbutouyDownloader():
     def get_resolutions_audio(self):
         if self.yt:
             self.streams = self.yt.streams.filter(only_audio=True)
-            res = [stream.abr for stream in self.streams if stream.abr is not None]
+            res = [stream.abr for stream in self.streams if stream.abr is not None and stream.abr != '160kbps' and stream.abr != '50kbps']
             return sorted(set(res), reverse=True)
         return None
 
@@ -59,6 +70,15 @@ class EbutouyDownloader():
             return True
         return None
 
+    def meta(self):
+        safe_title = re.sub(r'[/\\:<>"|?*]', '', self.yt.title)
+        m4a_path = f"{self.path}/{safe_title}.m4a"
+
+        audio = MP4(m4a_path)
+        title = f"{self.yt.title}"
+        audio["\xa9nam"] = [title]
+        audio["\xa9ART"] = [self.channel]
+        audio.save()
     def playlist_download(self):
         ...
 
@@ -70,6 +90,7 @@ def main():
 
     res = input("Enter resolution: ")
     downloader.single_audio_download(res)
+    downloader.meta()
 
 
 if __name__ == "__main__":
