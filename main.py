15cb82354126721d9eb515d5ef12eae840378378
AK <aamaal00777@gmail.com>
2026-04-10 14:43:57 +0400

    Completed the initial layout

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 import sys
+from urllib.request import urlopen
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QWidget, QApplication, QHBoxLayout, QVBoxLayout, QLabel, QPushButton, QListView, \
-    QListWidget, QComboBox, QFileDialog
+    QListWidget, QComboBox, QFileDialog, QLineEdit
 
 
 class MainWindow(QWidget):
@@ -16,6 +17,54 @@ class MainWindow(QWidget):
         self.setWindowTitle("Ebutouy")
         self.resize(600, 600)
         self.center()
+
+        main_layout = QVBoxLayout()
+        text_box_layout = QHBoxLayout()
+        video_box_layout = QHBoxLayout()
+        video_description_layout = QVBoxLayout()
+        download_btn_layout = QVBoxLayout()
+
+        heading = QLabel("Ebutouy")
+        url_box = QLineEdit()
+        search_btn = QPushButton("Get video")
+        download_mp3_btn = QPushButton("Download Audio")
+        download_mp4_btn = QPushButton("Download Video")
+        vid_thumbnail = QLabel(self)
+        url = 'https://static1.squarespace.com/static/5ceafa407824f80001793b84/5ceafcab6e9a7f68cda0e90b/5e3b3db0620bc34f20b7f7a4/1697669435090/some-any.jpg?format=1500w'
+        thumbnail_data = urlopen(url).read()
+        pixmap = QPixmap()
+        pixmap.loadFromData(thumbnail_data)
+        pixmap = pixmap.scaled(100, 100, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        resolution_combo_box = QComboBox()
+        resolution_combo_box.addItems(["360p", "480p"])
+
+        vid_thumbnail.setPixmap(pixmap)
+
+        vid_title = QLabel("Some vid")
+        vid_views = QLabel("1000")
+        vid_author = QLabel("AK")
+
+        video_box_layout.addWidget(vid_thumbnail)
+        video_box_layout.addLayout(video_description_layout)
+        video_box_layout.addLayout(download_btn_layout)
+        video_description_layout.addWidget(vid_title)
+        video_description_layout.addWidget(vid_views)
+        video_description_layout.addWidget(vid_author)
+        video_description_layout.addWidget(resolution_combo_box)
+        download_btn_layout.addWidget(download_mp3_btn)
+        download_btn_layout.addWidget(download_mp4_btn)
+
+
+        main_layout.addWidget(heading)
+        text_box_layout.addWidget(url_box)
+        text_box_layout.addWidget(search_btn)
+        main_layout.addLayout(text_box_layout)
+        main_layout.addLayout(video_box_layout)
+        self.setLayout(main_layout)
+
+
+
+
 
     def center(self) -> None:
         screen = QApplication.primaryScreen().availableGeometry()
